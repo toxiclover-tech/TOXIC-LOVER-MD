bd83fa866acf8b9b1020a10cdc5b90a8f6f4b08a
toxiclover-tech <Alonetech111@gmail.com>
2025-06-04 16:01:40 +0300

    Create wallpaper4.js

---
+++ luckycmd/wallpaper4.js
@@ -0,0 +1,22 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  'value': true
+});
+const {
+  ezra
+} = require("../fredi/ezra");
+ezra({
+  'nomCom': "wallpaper4",
+  'reaction': '🎞',
+  'nomFichier': __filename
+}, async (_0x4388c3, _0x2d564f, _0x51d5cb) => {
+  console.log("Commande saisie !!!s");
+  await _0x2d564f.sendMessage(_0x4388c3, {
+    'image': {
+      'url': 'https://files.catbox.moe/8cxyv5.jpg'
+    },
+    'caption': "🚗𝗥𝗮𝗻𝗱𝗼𝗺 𝘄𝗮𝗹𝗹𝗽𝗮𝗽𝗲𝗿 \n\n 🚘Download it and set it to your wallpaper 𝗙𝗿𝗼𝗺 (JEEPERS CREEPER-XMD)🚗MADE BY JEEPERS CREEPER-XMD tech"
+  });
+});
+console.log("mon test");
