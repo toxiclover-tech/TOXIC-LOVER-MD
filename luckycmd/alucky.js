f6174e7a07cab037730e37ecbda7886ac9ae6f94
toxiclover-tech <Alonetech111@gmail.com>
2025-06-05 16:33:51 +0300

    Create alucky.js

---
+++ luckycmd/alucky.js
@@ -0,0 +1,218 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+  'value': true
+});
+const {
+  ezra
+} = require("../fredi/ezra");
+ezra({
+  'nomCom': "wagroup",
+  'reaction': '🤨',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x3258e7, _0x4c4732, _0x13b70c) => {
+  console.log("Commande saisie !!!s");
+  await _0x4c4732.sendMessage(_0x3258e7, {
+    'text': "Hello 👋\n\nClick on the button below to join the OFFICIAL *JEEPERS CREEPER-XMD* WhatsApp Gruop",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': "",
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/f0f1wg.jpg",
+        'title': "Join Our WhatsApp Group",
+        'body': "Click to join the official JEEPERS CREEPER-XMD WhatsApp group!"
+      }
+    }
+  });
+  console.log("Command executed: wagroup");
+});
+ezra({
+  'nomCom': 'wachannel',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the button below to Follow the OFFICIAL *JEEPERS CREEPERS-XMD* WhatsApp Channel",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://whatsapp.com/channel/0029VawCel7GOj9ktLjkxQ3g',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/4g6ty3.jpg",
+        'title': "Join Our WhatsApp Channel",
+        'body': "Click to join the official JEEPERS CREEPER-XMD WhatsApp channel!"
+      }
+    }
+  });
+  console.log("Command executed: wachannel");
+});
+ezra({
+  'nomCom': 'waowner',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the button below to contact the OFFICIAL *JEEPERS CREEPER-XMD* Owner",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://wa.link/3rc9x7',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/lcori6.jpg",
+        'title': "Join Our Developer Place",
+        'body': "Click to join the official JEEPERS CREEPER-XMD Owner Inbox!"
+      }
+    }
+  });
+  console.log("Command executed: waowner");
+});
+ezra({
+  'nomCom': 'fb-page',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Follow the OFFICIAL *JEEPERS CREEPER-XMD* Facebook Page",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.facebook.com/bravin126',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/7a00n2.jpg",
+        'title': "Follow Facebook Page 📄",
+        'body': "Click to join the official Jeepers creepers xmd Facebook Page!"
+      }
+    }
+  });
+  console.log("Command executed: fb-page");
+});
+ezra({
+  'nomCom': 'insta-page',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Follow the OFFICIAL *JEEPERS CREEPER-XMD* Instagram Page",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.instagram.com/bravin126?igsh=MWJvZzBnN3RsYTRsag==',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/ivgtst.jpg",
+        'title': "Follow Instagram Page 📄",
+        'body': "Click to join the official JEEPERS  CREEPERS-XMD Instagram Page!"
+      }
+    }
+  });
+  console.log("Command executed: insta-page");
+});
+ezra({
+  'nomCom': 'threads-page',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Follow the OFFICIAL *JEEPERS CREEPER-XMD* Threads Page",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://www.threads.n
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/28gqcp.jpg"
+        'title': "Follow Threads Page 📄",
+        'body': "Click to join the official JEEPERS CREEPER-XMD Threads Page!"
+      }
+    }
+  });
+  console.log("Command executed: threads-page");
+});
+ezra({
+  'nomCom': 'tiktok-page',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Follow the OFFICIAL *JEEPERS CREEPER-XMD* TikTok Page",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://vm.tiktok.com/ZMSB9Lagc/',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/lvvceg.jpg",
+        'title': "Follow TikTok Page 📄",
+        'body': "Click to join the official TOXIC LOVER-MD TikTok Page!"
+      }
+    }
+  });
+  console.log("Command executed: tiktok-page");
+});
+ezra({
+  'nomCom': 'tgroup',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Join the OFFICIAL *JEEPERS CREEPER-XMD" Telegram Group",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://t.me/+u3zh',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/7a00n2.jpg",
+        'title': "Join Telegram Group📄",
+        'body': "Click to join the official JEEPERS CREEPER-XMD Telegram Group!"
+      }
+    }
+  });
+  console.log("Command executed: tgroup");
+});
+ezra({
+  'nomCom': 'ytchannel',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to Subscribe the OFFICIAL *JEEPERS CREEPER-XMD* YouTube Channel",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://youtube.com/@alonetech-d1u?si=fKytTd4UolhKbaNs',
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/g2ln7g.jpg",
+        'title': "Follow YouTube Channel📄",
+        'body': "Click to Subscribe the official JEEPERS CREEPERS-XMD YouTube Channel!"
+      }
+    }
+  });
+  console.log("Command executed: ytchannel");
+});
+ezra({
+  'nomCom': 't-help',
+  'reaction': '👀',
+  'categorie': "Support-Owner",
+  'nomFichier': __filename
+}, async (_0x14c950, _0x346e6b, _0x31cbea) => {
+  console.log("Commande saisie !!!s");
+  await _0x346e6b.sendMessage(_0x14c950, {
+    'text': "Hello 👋\n\nClick on the photo below to connect the OFFICIAL *JEEPERS CREEPER-XMD* Telegram Inbox",
+    'contextInfo': {
+      'externalAdReply': {
+        'sourceUrl': 'https://t.me/,
+        'mediaType': 0x1,
+        'mediaUrl': "https://files.catbox.moe/lvvceg.jpg",
+        'title': "Chating With Owner",
+        'body': "Click to Contact the official JEEPERS CREEPERS-XMD Telegram Inbox!"
+      }
+    }
+  });
+  console.log("Command executed: t-help");
+});
