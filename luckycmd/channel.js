997b8a3257f70a143382f1415132f5268c384849
toxiclover-tech <Alonetech111@gmail.com>
2025-06-05 12:53:31 +0300

    Create channel.js

---
+++ luckycmd/channel.js
@@ -0,0 +1,31 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { ezra } = require("../fredi/ezra");
+
+ezra({ nomCom: "channel", reaction: "💐", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
+    console.log("Commande saisie !!!s");
+    let z = 'Salut je m\'appelle JEEPERS CREEPER-XMD* \n\n ' + 'je suis un bot Whatsapp Multi-appareil voici la chaîne';
+    let d = ' developpé par *toxic lover*';
+    let varmess = z + d;
+    var lien = 'https://files.catbox.moe/wa4jj9.jpg';  // Remplacez cet URL par le lien que vous souhaitez envoyer
+    await zk.sendMessage(dest, { text: varmess + "\n" + lien });
+});
+
+console.log("mon test");
+
+;
+console.log("mon test");
+/*module.exports.commande = () => {
+  var nomCom = ["test","t"]
+  var reaction="☺️"
+  return { nomCom, execute,reaction }
+};
+
+async function  execute  (origineMessage,zok) {
+  console.log("Commande saisie !!!s")
+   let z ='Salut je m\'appelle *JEEPERS CREEPER-XMD* \n\n '+'je suis un bot Whatsapp Multi-appareil '
+      let d =' developpé par *Fredi*'
+      let varmess=z+d
+      var img='https://files.catbox.moe/wa4jj9.jpg'
+await  zok.sendMessage(origineMessage,  { image:{url:img},caption:varmess});
+}  */ 
