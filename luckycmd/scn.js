f26ba29e6f990653b0bae638b922416fd99ecddf
JEEPERS CREEPER-XMD <Alonetech111@gmail.com>
2025-06-14 10:30:35 +0300

    Update scn.js

--- luckycmd/scn.js
+++ luckycmd/scn.js
@@ -1,3 +1,72 @@
-
-
-const _0x47f68c=_0x3a17;function _0x3427(){const _0x81cf85=['temps\x20','https://telegra.ph/file/df50557cf503c18190a2f.jpg','8766nfReqt','420EAxsDs','/../fredi/ezra','31932770kGKjIn','moment-timezone','DD/MM/YYYY','2vcKRmP','asia/karachi','map','date','push','deploy','🥵🥵\x20Menu\x20erreur\x20','\x0a*𝙳𝙴𝙿𝙻𝙾𝚈\x20𝚈𝙾𝚄𝚁\x20𝙾𝚆𝙽\x20JEEPERS\x20CREEPER\x20🤦\x20𝙷𝙴𝚁𝙴*\x0a𝚈𝙾𝚄𝚁\x20𝙳𝙴𝙿𝙻𝙾𝚈𝙴𝚁\x20:\x20','454404VrHzUk','2741280rYekmP','7niFaxj','sendMessage','format','nomCom','setDefault','651091ALLXVi','OWNER_NAME','log','\x0a*𝚃𝙾𝚃𝙰𝙻\x20𝐋𝐔𝐂𝐊𝐘\x20𝙼𝙳\x20𝚄𝚂𝙴𝚁𝚂\x20:\x20𝟷𝟶𝟾𝟿+𝚄𝚜𝚎𝚛𝚜*\x0a𝙱𝙾𝚃\x20𝙳𝙴𝙾𝙻𝙾𝚈𝙴𝙳\x20:\x20𝟷𝟹𝟿+\x20𝚊𝚙𝚙𝚜\x20𝚍𝚎𝚙𝚕𝚘𝚢𝚎𝚍\x20𝚝𝚘𝚍𝚊𝚢\x0a*𝚃𝙾𝙿\x20𝙾𝙽\x20𝚃𝙷𝙴\x20𝙻𝙸𝙽𝙺\x20𝚃𝙾\x20𝙶𝙴𝚃\x20𝚂𝙴𝚂𝚂𝙸𝙾𝙽*\x0a\x0ahttps://davincs-id.onrender.com\x0a\x0a\x0a*𝚂𝚃𝙴𝙿𝚂\x20𝚃𝙾\x20𝙶𝙴𝚃\x20𝚂𝙴𝚂𝚂𝙸𝙾𝙽\x20𝙸𝙳*\x0a*𝟷.𝙾𝙿𝙴𝙽\x20𝙻𝙸𝙽𝙺\x20𝙰𝙱𝙾𝚅𝙴*\x0a*𝟸.𝙸𝙽𝚃𝙴𝚁\x20𝚈𝙾𝚄𝚁\x20𝚆𝙷𝙰𝚃𝚂𝙰𝙰𝙿\x20𝙽𝚄𝙼𝙱𝙴𝚁*\x20*𝚆𝙸𝚃𝙷*\x0a*𝙲𝙾𝚄𝙽𝚃𝚈\x20𝙲𝙾𝙳𝙴\x20𝙴𝚐.\x20𝟸𝟻𝟻𝟽52593𝚡𝚡𝚡*\x0a*𝟹.DAVINCS\x20𝚆𝙸𝙻𝙻\x20𝚂𝙴𝙽𝙳\x20𝚈𝙾𝚄\x20𝙰\x20𝙲𝙾𝙳𝙴\x20𝙲𝙾𝙿𝚈*\x0a\x20*𝚃𝙷𝙰𝚃\x20𝙲𝙾𝙳𝙴.\x20𝚃𝙷𝙴𝙽\x20𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿\x20𝚆𝙸𝙻𝙻*\x20*𝚂𝙴𝙽𝚃*\x0a\x20*𝟐𝟓𝟓𝟔𝟐𝟎𝟖𝟏𝟒𝐱𝐱𝐱.*\x0a*𝟺.𝚃𝙾𝙿\x20𝙾𝙽\x20𝚃𝙷𝙰𝚃\x20𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍*\x20*DAVINCS\x20𝙸𝙽𝚃𝙴𝚁*\x20\x0a\x20\x20*𝚃𝙷𝙴\x20𝙲𝙾𝙳𝙴\x20𝚃𝙷𝙰𝚃\x2DAVINCS\x20𝙼𝙳\x20𝚂𝙴𝙽𝚃\x20𝚈𝙾𝚄*\x0a*𝟻.𝙸𝚃\x20𝚆𝙸𝙻𝙻\x20𝙻𝙾𝙰𝙳\x20𝙵𝙾𝚁\x20𝚂𝙾𝙼𝙴𝚃𝙸𝙼𝙴*\x20*𝚃𝙷𝙴𝙽\x20𝐋𝐔𝐂𝐊𝐘\x20𝙼𝙳*\x0a\x20\x20*𝚆𝙸𝙻𝙻\x20𝚂𝙴𝙽𝙳\x20𝚈𝙾𝚄\x20𝙰\x20𝚂𝙴𝚂𝚂𝙸𝙾𝙽\x20𝙸𝙳\x20𝙸𝙽*\x20*𝚈𝙾𝚄𝚁*\x0a\x20\x20*𝙸𝙽𝙱𝙾𝚇\x20𝙸𝙽\x20𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿\x20𝙰𝚃\x20𝚈𝙾𝚄𝚁\x20𝙾𝚆𝙽*\x20*𝙽𝚄𝙼𝙱𝙴𝚁*\x0a\x20\x20*𝙲𝙾𝙿𝚈\x20𝚃𝙷𝙴\x20𝚂𝙴𝚂𝚂𝙸𝙾𝙽\x20𝙸𝙳\x20𝙰𝙽𝙳\x20𝚂𝙴𝙽𝙳*\x20*𝚃𝙾\x20𝚈𝙾ur\x20𝙳𝙴𝙿𝙻𝙾𝚈𝙴𝚁*\x0a\x20\x20\x20\x0a\x20\x20\x20𝙽𝚘𝚝𝚎\x20:\x20𝚋𝚘𝚝\x20𝚊𝚛𝚎𝚗\x27𝚝\x20𝚏𝚘𝚛\x20𝚏𝚛𝚎𝚎\x0a\x20\x20\x20\x0a\x20\x20\x20𝚔𝚎𝚎𝚙\x20𝚞𝚜𝚒𝚗𝚐\x20DAVINCS\x20𝚖𝚍\x0a\x0a','5732800QeIsGH','/../fredi//ezra','util','/../fredi/mesfonctions','oui','©TKM\x20INC','HH:MM:SS','/../set','locale','5574303FNerNY','MODE_PUBLIC'];_0x3427=function(){return _0x81cf85;};return _0x3427();}function _0x3a17(_0x320f8c,_0x4b6d99){const _0x34275a=_0x3427();return _0x3a17=function(_0x3a1776,_0x4329cd){_0x3a1776=_0x3a1776-0xa2;let _0x3faab5=_0x34275a[_0x3a1776];return _0x3faab5;},_0x3a17(_0x320f8c,_0x4b6d99);}(function(_0x1b9ba1,_0x133efd){const _0x357c63=_0x3a17,_0x34cf5b=_0x1b9ba1();while(!![]){try{const _0xbbba64=parseInt(_0x357c63(0xae))/0x1*(-parseInt(_0x357c63(0xc5))/0x2)+-parseInt(_0x357c63(0xbf))/0x3*(parseInt(_0x357c63(0xc0))/0x4)+-parseInt(_0x357c63(0xa8))/0x5+parseInt(_0x357c63(0xa7))/0x6*(parseInt(_0x357c63(0xa9))/0x7)+-parseInt(_0x357c63(0xb2))/0x8+-parseInt(_0x357c63(0xbb))/0x9+parseInt(_0x357c63(0xc2))/0xa;if(_0xbbba64===_0x133efd)break;else _0x34cf5b['push'](_0x34cf5b['shift']());}catch(_0x56dd49){_0x34cf5b['push'](_0x34cf5b['shift']());}}}(_0x3427,0x68387));const util=require(_0x47f68c(0xb4)),fs=require('fs-extra'),{ezra}=require(__dirname+_0x47f68c(0xc1)),{format,styletext}=require(__dirname+_0x47f68c(0xb5)),os=require('os'),moment=require(_0x47f68c(0xc3)),s=require(__dirname+_0x47f68c(0xb9));ezra({'nomCom':_0x47f68c(0xa4),'categorie':_0x47f68c(0xa4)},async(_0x1b122e,_0x28446a,_0x43827c)=>{const _0x40bc8f=_0x47f68c;let {ms:_0xba9598,repondre:_0xa253d5}=_0x43827c,{cm:_0x3eefe7}=require(__dirname+_0x40bc8f(0xb3));var _0x119fe5={},_0x4309cb='public';s[_0x40bc8f(0xbc)]!=_0x40bc8f(0xb6)&&(_0x4309cb='private');var _0x43801a={'Général':'','Logo':'','hentai':'','weeb':'','Recherche':'','conversion':'','groupe':'','Téléchargement':''};_0x3eefe7[_0x40bc8f(0xc7)](async(_0xc32296,_0x3daccf)=>{const _0x3238af=_0x40bc8f;if(!_0x119fe5[_0xc32296['categorie']])_0x119fe5[_0xc32296['categorie']]=[];_0x119fe5[_0xc32296['categorie']][_0x3238af(0xa3)](_0xc32296[_0x3238af(0xac)]);});const _0x455992=moment(moment())[_0x40bc8f(0xab)](_0x40bc8f(0xb8));moment['tz'][_0x40bc8f(0xad)]('asia/karachi\x20')[_0x40bc8f(0xba)]('id');const _0x1e5e1a=moment['tz'](_0x40bc8f(0xc6))[_0x40bc8f(0xab)](_0x40bc8f(0xc4));console['log'](_0x40bc8f(0xa2)+_0x1e5e1a),console['log'](_0x40bc8f(0xbd)+_0x455992);let _0x2d2a24='\x20*𝙳𝙴𝙿𝙻𝙾𝚈\x20𝚈𝙾𝚄𝚁\x20𝙾𝚆𝙽\x20𝐋𝐔𝐂𝐊𝐘\x20𝙼𝙳*\x0a\x0a';_0x2d2a24+='\x0a𝙷𝙴𝙻𝙻𝙾𝚆\x20:\x20'+_0xba9598['pushName']+_0x40bc8f(0xa6)+s[_0x40bc8f(0xaf)]+'\x0a𝚈𝙾𝚄𝚁\x20𝙽𝙰𝙼𝙴\x20:\x20'+_0xba9598['pushName']+_0x40bc8f(0xb1);for(const _0x115688 in _0x119fe5){!_0x43801a[_0x115688]&&(_0x43801a[_0x115688]='');_0x2d2a24+='';for(const _0x344838 of _0x119fe5[_0x115688]){_0x2d2a24+='';}}var _0x46fca1=_0x40bc8f(0xbe);try{_0x28446a[_0x40bc8f(0xaa)](_0x1b122e,{'image':{'url':_0x46fca1},'caption':_0x2d2a24,'footer':_0x40bc8f(0xb7)},{'quoted':_0xba9598});}catch(_0x193aab){console[_0x40bc8f(0xb0)](_0x40bc8f(0xa5)+_0x193aab),_0xa253d5(_0x40bc8f(0xa5)+_0x193aab);}});
+const {
+  ezra
+} = require(__dirname + "/../fredi/ezra");
+const {
+  format,
+  styletext
+} = require(__dirname + "/../fredi/mesfonctions");
+const moment = require("moment-timezone");
+const s = require(__dirname + "/../set");
+ezra({
+  'nomCom': "deploy",
+  'categorie': "deploy"
+}, async (_0x1b122e, _0x28446a, _0x43827c) => {
+  let {
+    ms: _0xba9598,
+    repondre: _0xa253d5
+  } = _0x43827c;
+  let {
+    cm: _0x3eefe7
+  } = require(__dirname + "/../fredi//ezra");
+  var _0x119fe5 = {};
+  var _0x4309cb = 'public';
+  if (s.MODE_PUBLIC != "oui") {
+    _0x4309cb = 'private';
+  }
+  var _0x43801a = {
+    'Général': '',
+    'Logo': '',
+    'hentai': '',
+    'weeb': '',
+    'Recherche': '',
+    'conversion': '',
+    'groupe': '',
+    'Téléchargement': ''
+  };
+  _0x3eefe7.map(async (_0xc32296, _0x3daccf) => {
+    if (!_0x119fe5[_0xc32296.categorie]) {
+      _0x119fe5[_0xc32296.categorie] = [];
+    }
+    _0x119fe5[_0xc32296.categorie].push(_0xc32296.nomCom);
+  });
+  const _0x455992 = moment(moment()).format("HH:MM:SS");
+  moment.tz.setDefault("asia/karachi ").locale('id');
+  const _0x1e5e1a = moment.tz("asia/karachi").format("DD/MM/YYYY");
+  console.log("date" + _0x1e5e1a);
+  console.log("temps " + _0x455992);
+  let _0x2d2a24 = " *𝙳𝙴𝙿𝙻𝙾𝚈 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 JEEPERS CREEPER-XMD *\n\n";
+  _0x2d2a24 += "\n𝙷𝙴𝙻𝙻𝙾𝚆 : " + _0xba9598.pushName + "\n*𝙳𝙴𝙿𝙻𝙾𝚈 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽 JEEPERS CREEPER-XMD🤦 𝙷𝙴𝚁𝙴*\n𝚈𝙾𝚄𝚁 𝙳𝙴𝙿𝙻𝙾𝚈𝙴𝚁 : " + s.OWNER_NAME + "\n𝚈𝙾𝚄𝚁 𝙽𝙰𝙼𝙴 : " + _0xba9598.pushName + "\n*𝚃𝙾𝚃𝙰𝙻 JEEPERS CREEPER-XMD 𝚄𝚂𝙴𝚁𝚂 : 𝟷𝟶𝟾𝟿+𝚄𝚜𝚎𝚛𝚜*\n𝙱𝙾𝚃 𝙳𝙴𝙾𝙻𝙾𝚈𝙴𝙳 : 𝟷𝟹𝟿+ 𝚊𝚙𝚙𝚜 𝚍𝚎𝚙𝚕𝚘𝚢𝚎𝚍 𝚝𝚘𝚍𝚊𝚢\n*𝚃𝙾𝙿 𝙾𝙽 𝚃𝙷𝙴 𝙻𝙸𝙽𝙺 𝚃𝙾 𝙶𝙴𝚃 𝚂𝙴𝚂𝚂𝙸𝙾𝙽*\n\nhttps://jeepers-creepers-xmd-session-link-1.onrender.com/\n\n\n*𝚂𝚃𝙴𝙿𝚂 𝚃𝙾 𝙶𝙴𝚃 𝚂𝙴𝚂𝚂𝙸𝙾𝙽 𝙸𝙳*\n*𝟷.𝙾𝙿𝙴𝙽 𝙻𝙸𝙽𝙺 𝙰𝙱𝙾𝚅𝙴*\n*𝟸.𝙸𝙽𝚃𝙴𝚁 𝚈𝙾𝚄𝚁 𝚆𝙷𝙰𝚃𝚂𝙰𝙰𝙿 𝙽𝚄𝙼𝙱𝙴𝚁* *𝚆𝙸𝚃𝙷*\n*𝙲𝙾𝚄𝙽𝚃𝚈 𝙲𝙾𝙳𝙴 𝙴𝚐. 𝟸𝟻4717263𝚡𝚡𝚡*\n*𝟹.JEEPERS CREEPER-XMD 𝚆𝙸𝙻𝙻 𝚂𝙴𝙽𝙳 𝚈𝙾𝚄 𝙰 𝙲𝙾𝙳𝙴 𝙲𝙾𝙿𝚈*\n *𝚃𝙷𝙰𝚃 𝙲𝙾𝙳𝙴. 𝚃𝙷𝙴𝙽 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝚆𝙸𝙻𝙻* *𝚂𝙴𝙽𝚃*\n *𝟐𝟓47172636𝐱𝐱𝐱.*\n*𝟺.𝚃𝙾𝙿 𝙾𝙽 𝚃𝙷𝙰𝚃 𝐍𝐎𝐓𝐈𝐅𝐈𝐂𝐀𝐓𝐈𝐎𝐍* *JEEPERS CREEPER-XMD 𝙸𝙽𝚃𝙴𝚁* \n  *𝚃𝙷𝙴 𝙲𝙾𝙳𝙴 𝚃𝙷𝙰𝚃-JEEPERS CREEPER-XMD 𝚂𝙴𝙽𝚃 𝚈𝙾𝚄*\n*𝟻.𝙸𝚃 𝚆𝙸𝙻𝙻 𝙻𝙾𝙰𝙳 𝙵𝙾𝚁 𝚂𝙾𝙼𝙴𝚃𝙸𝙼𝙴* *𝚃𝙷𝙴𝙽 JEEPERS CREEPER-XMD*\n  *𝚆𝙸𝙻𝙻 𝚂𝙴𝙽𝙳 𝚈𝙾𝚄 𝙰 𝚂𝙴𝚂𝚂𝙸𝙾𝙽 𝙸𝙳 𝙸𝙽* *𝚈𝙾𝚄𝚁*\n  *𝙸𝙽𝙱𝙾𝚇 𝙸𝙽 𝚆𝙷𝙰𝚃𝚂𝙰𝙿𝙿 𝙰𝚃 𝚈𝙾𝚄𝚁 𝙾𝚆𝙽* *𝙽𝚄𝙼𝙱𝙴𝚁*\n  *𝙲𝙾𝙿𝚈 𝚃𝙷𝙴 𝚂𝙴𝚂𝚂𝙸𝙾𝙽 𝙸𝙳 𝙰𝙽𝙳 𝚂𝙴𝙽𝙳* *𝚃𝙾 𝚈𝙾ur 𝙳𝙴𝙿𝙻𝙾𝚈𝙴𝚁*\n   \n   𝙽𝚘𝚝𝚎 : 𝚋𝚘𝚝 𝚊𝚛𝚎𝚗'𝚝 𝚏𝚘𝚛 𝚏𝚛𝚎𝚎\n   \n   𝚔𝚎𝚎𝚙 𝚞𝚜𝚒𝚗𝚐 JEEPERS CREEPER-XMD \n\n";
+  for (const _0x115688 in _0x119fe5) {
+    if (!_0x43801a[_0x115688]) {
+      _0x43801a[_0x115688] = '';
+    }
+    _0x2d2a24 += '';
+    for (const _0x344838 of _0x119fe5[_0x115688]) {
+      _0x2d2a24 += '';
+    }
+  }
+  try {
+    _0x28446a.sendMessage(_0x1b122e, {
+      'image': {
+        'url': "https://files.catbox.moe/tq1dtv.jpg"
+      },
+      'caption': _0x2d2a24,
+      'footer': "©TKM INC"
+    }, {
+      'quoted': _0xba9598
+    });
+  } catch (_0x193aab) {
+    console.log("🥵🥵 Menu erreur " + _0x193aab);
+    _0xa253d5("🥵🥵 Menu erreur " + _0x193aab);
+  }
+});
