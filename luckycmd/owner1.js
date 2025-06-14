bd0a5eba0fa5f707b7202cd3089672fc0c11871d
JEEPERS CREEPER-XMD <Alonetech111@gmail.com>
2025-06-14 11:37:19 +0300

    Update owner1.js

--- luckycmd/owner1.js
+++ luckycmd/owner1.js
@@ -37,7 +37,7 @@ const date = moment().format('DD/MM/YYYY');
 
    if (lien.match(/\.(mp4|gif)$/i)) {
     try {
-        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *Davincs Md*, déveloper JEEPERS CREEPER-XMD Tech" , gifPlayback : true }, { quoted: ms });
+        zk.sendMessage(dest, { video: { url: lien }, caption:infoMsg + menuMsg, footer: "Je suis *JEEPERS CREEPER-XMD*, déveloper JEEPERS CREEPER-XMD Tech" , gifPlayback : true }, { quoted: ms });
     }
     catch (e) {
         console.log("🥵🥵 Menu erreur " + e);
